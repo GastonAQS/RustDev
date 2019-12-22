7a1bc84427758d5fe05c591a657090193a4c6211
GastonAQS <31553863+GastonAQS@users.noreply.github.com>
2019-12-22 12:56:15 -0300

    workspace chapter

---
+++ adder2/add-one/src/lib.rs
@@ -0,0 +1,17 @@
+extern crate rand;
+
+
+pub fn add_one(x: u32) -> u32 {
+    x+1
+}
+
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn it_works() {
+        assert_eq!(4, add_one(3));
+    }
+}
---
+++ adder2/src/main.rs
@@ -0,0 +1,7 @@
+extern crate add_one;
+
+
+fn main() {
+    let sum = 10;
+    println!("Hey, {} plus one is {}",sum,add_one::add_one(sum))
+}
